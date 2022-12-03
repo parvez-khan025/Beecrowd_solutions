9fa553777f9072ba7eaa40a067e6a1b9be141f55
Md Parvez Khan <117196251+parvez-khan025@users.noreply.github.com>
2022-12-03 10:54:02 +0600

    Create 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      double R,x;
+      cin>>R;
+      x=3.14159*R*R;
+
+       cout<<"A="<<fixed<<setprecision(4)<<x<<endl;
+
+        return 0;
+}
