139a34133ca07a4ecb4f70952dc7411138b0a71d
Md Parvez Khan <117196251+parvez-khan025@users.noreply.github.com>
2022-12-04 13:48:02 +0600

    Create 1005.cpp

---
+++ 1005.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a, b,c;
+    cin>>a>>b;
+    c=(a*3.5+b*7.5)/(3.5+7.5);
+    cout<<"MEDIA = "<<fixed<<setprecision(5)<<c<<endl;
+    
+
+  return 0;
+}
