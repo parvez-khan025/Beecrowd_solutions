1bef2af0c9aebbbc329e5ebb21b13594ca955b09
Md Parvez Khan <117196251+parvez-khan025@users.noreply.github.com>
2022-12-18 12:07:30 +0600

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	double a,b,c,d;
+	cin>>a>>b>>c;
+	d=(a*2+b*3+c*5)/10;
+
+	cout<<"MEDIA = "<<fixed<<setprecision(1)<<d<<endl;
+
+	return 0;
+}
