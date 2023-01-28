67bacc7147f17d46ecc051d0b0f16bbf35c3d09c
Md Parvez Khan <117196251+parvez-khan025@users.noreply.github.com>
2023-01-28 14:11:06 +0600

    Create 1008.cpp

---
+++ 1008.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    int a, b;
+    float c, d;
+    
+    cin>>a>>b;
+    cin>>c;
+    d=b*c;
+
+    cout<<"NUMBER = "<<a<<endl;
+    cout<<"SALARY = U$ "<<fixed<<setprecision(2)<<d<<endl;
+
+    return 0;
+}
