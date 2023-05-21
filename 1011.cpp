e5940077c8c5f40e5844e7c1df38abc6d0350048
Md Parvez Khan <117196251+parvez-khan025@users.noreply.github.com>
2023-05-21 13:56:31 +0600

    Create 1011.cpp

---
+++ 1011.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+    {
+			double R, ans;
+			
+			cin>>R;
+
+			ans=(4.0/3.0)*3.14159*R*R*R;
+
+        cout<<fixed<<setprecision(3)<<"VOLUME = "<<ans<<endl;
+
+
+
+        return 0; 
+
+    }
